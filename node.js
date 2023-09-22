3c5f41bea49d29b81214c12ca491d2d26cb5609a
josipcodes <j.potnar@gmail.com>
2023-09-22 15:26:48 +0100

    Solve destructuring challenge

---
+++ node.js
@@ -0,0 +1,10 @@
+let mixedLetters = ['b', 'd', 'a', 'c', 'f', 'e'];
+
+let moreMixedLetters = [...mixedLetters, "h", "k", "g", "j", "i", "l"]
+console.log(moreMixedLetters)
+
+const updateSortReverse = (arr, ...letters) => [...arr, ...letters].sort().reverse();
+
+let reverseSort = updateSortReverse(moreMixedLetters, "n", "m", "o")
+console.log(reverseSort)
+console.log(mixedLetters)
